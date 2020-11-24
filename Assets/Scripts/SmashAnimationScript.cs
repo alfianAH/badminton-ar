84c48ee9f681a73379d347e9e59ee3b8071e4f4c
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-11-24 11:02:04 +0800

    Play animation and show fire efect through script

---
+++ Assets/Scripts/SmashAnimationScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SmashAnimationScript : MonoBehaviour
+{
+    [SerializeField] private GameObject fireParticle;
+    
+    private Animator smashAnimator;
+
+    private void Start()
+    {
+        smashAnimator = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        StartCoroutine(Delay(3f));
+    }
+
+    public void ShowFireEffect()
+    {
+        fireParticle.SetActive(true);
+    }
+
+    private IEnumerator Delay(float second)
+    {
+        yield return new WaitForSeconds(second);
+        smashAnimator.SetTrigger("PlayAnim");
+    }
+}
